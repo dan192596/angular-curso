53edcc25f193b6470938dcb5f9f7e6940978fd6b
Daniel Perez <daniel.perez@acreditame.com>
2022-08-30 05:17:04 -0600

    componentes

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Personaje } from '../interfaces/dbz.interface';
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
 
-  constructor() { }
+  public personajes:Personaje[]=[
+    {
+      nombre:'Goku',
+      poder:15000
+    },
+    {
+      nombre:'Vegeta',
+      poder: 7500
+    }
+  ];
 
-  ngOnInit(): void {
+  public nuevo:Personaje ={
+    nombre:'',
+    poder: 0
+  }
+
+  agregar(){
+    if(this.nuevo.nombre.trim().length ===0){return;}
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre:'',
+      poder:0
+    }
+    return;
+    // console.log(this.nuevo)
   }
 
 }
