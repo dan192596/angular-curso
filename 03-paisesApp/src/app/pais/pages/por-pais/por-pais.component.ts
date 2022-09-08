83f56e656602197b25289e7eb0f3c016d8285769
Daniel Perez <daniel.perez@acreditame.com>
2022-09-07 22:04:52 -0600

    busqueda por pais

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -6,11 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
 
-  constructor() { }
+  termino:string = '';
+  hayError: boolean = false;
+  paises : Country[] = [];
 
-  ngOnInit(): void {
+  constructor(private paisService: PaisService) { }
+
+  buscar(){
+    this.hayError=false;
+    console.log(this.termino);
+
+    this.paisService.buscarPais(this.termino)
+      .subscribe(resp =>{
+        console.log(resp)
+        this.paises = resp;
+      }, (err)=>{        
+        this.hayError=true;
+        this.paises=[];
+      });
   }
 
 }
