f2e7bac947aa19128fa6b5b183f9093c3b2ab288
Daniel Perez <daniel.perez@acreditame.com>
2022-09-08 20:56:21 -0600

    inputs y tabla

---
+++ 03-paisesApp/src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pas-input',
+  templateUrl: './pais-input.component.html',
+  styles: [
+  ]
+})
+export class PaisInputComponent  {
+
+  termino: string = '';
+
+  buscar(){
+    
+  }
+
+}
