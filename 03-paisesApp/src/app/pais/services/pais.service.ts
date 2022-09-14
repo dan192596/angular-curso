98442a7e88e4cfa95e7174e2fc78ea059747ef5b
Daniel Perez <daniel.perez@acreditame.com>
2022-09-14 11:43:09 -0600

    por region

--- 03-paisesApp/src/app/pais/services/pais.service.ts
+++ 03-paisesApp/src/app/pais/services/pais.service.ts
@@ -26,5 +26,10 @@ export class PaisService {
     const url = `${this.apiUrl}/alpha/${id}`;
     return this.http.get<Country[]>(url);
   }
+
+  buscarRegion(region: string): Observable<Country[]>{
+    const url = `${this.apiUrl}/region/${region}`;
+    return this.http.get<Country[]>(url);    
+  }
    
 }
