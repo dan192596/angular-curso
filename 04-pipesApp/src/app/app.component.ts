1697830631772b7146d032ef249c4c69e680391c
Daniel Perez <daniel.perez@acreditame.com>
2022-09-19 21:40:55 -0600

    prime ng

--- 04-pipesApp/src/app/app.component.ts
+++ 04-pipesApp/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+
+  nombre: string = 'Fernando heRrera';
+
+  mostrarNombre(){
+    console.log(this.nombre);
+  }
 }
